5a9b85aaaef9720f149e07121e666daf53e0e330
ToruNiina <niina.toru.68u@gmail.com>
2017-01-11 15:54:40 +0900

    use async and future instead of thread

--- mjolnir/core/LocalForceField.hpp
+++ mjolnir/core/LocalForceField.hpp
@@ -12,7 +12,8 @@
 
 #ifdef MJOLNIR_PARALLEL_THREAD
 #include <thread>
-#include <algorithm>
+#include <future>
+#include <mutex>
 #endif
 
 namespace mjolnir
@@ -44,12 +45,7 @@ class LocalForceField
   
   public:
 
-#ifdef MJOLNIR_PARALLEL_THREAD
-    LocalForceField() : num_threads(1){}
-    LocalForceField(const std::size_t th) : num_threads(th){}
-#else
     LocalForceField() = default;
-#endif
 
     ~LocalForceField() = default;
     LocalForceField(LocalForceField const&) = delete;
@@ -63,21 +59,28 @@ class LocalForceField
     void emplace_dihedral(std::size_t i, std::size_t j, std::size_t k,
             std::size_t l, potential_ptr&& pot);
 
-    void      calc_force(particle_container_type& pcon);
+    void      calc_force(particle_container_type& pcon) const;
     real_type calc_energy(const particle_container_type& pcon) const;
+
+#ifdef MJOLNIR_PARALLEL_THREAD
+    void calc_force(particle_container_type& pcon,
+                    const std::size_t num_threads) const;
+    real_type calc_energy(const particle_container_type& pcon,
+                          const std::size_t num_threads) const;
+#endif
 
     void
     calc_bond_force(particle_container_type& pcon,
                     typename bond_container::const_iterator iter,
-                    const typename bond_container::const_iterator end);
+                    const typename bond_container::const_iterator end) const;
     void
     calc_angle_force(particle_container_type& pcon,
                      typename angle_container::const_iterator iter,
-                     const typename angle_container::const_iterator end);
+                     const typename angle_container::const_iterator end) const;
     void
     calc_dihd_force(particle_container_type& pcon,
                     typename dihd_container::const_iterator iter,
-                    const typename dihd_container::const_iterator end);
+                    const typename dihd_container::const_iterator end) const;
 
     real_type
     calc_bond_energy(const particle_container_type& pcon,
@@ -92,16 +95,7 @@ class LocalForceField
                      typename dihd_container::const_iterator iter,
                      const typename dihd_container::const_iterator end) const;
 
-#ifdef MJOLNIR_PARALLEL_THREAD
-    std::size_t&       threads()       {return num_threads;}
-    std::size_t const& threads() const {return num_threads;}
-#endif
-
   private:
-
-#ifdef MJOLNIR_PARALLEL_THREAD
-    std::size_t num_threads;
-#endif
 
     BondLengthInteraction<traitsT>    bond;
     BondAngleInteraction<traitsT>     angle;
@@ -186,141 +180,15 @@ void LocalForceField<traitsT>::emplace_dihedral(
     return ;
 }
 
-#ifdef MJOLNIR_PARALLEL_THREAD
 template<typename traitsT>
-void LocalForceField<traitsT>::calc_force(particle_container_type& pcon)
-{
-// bond ------------------------------------------------------------------------
-    const std::size_t bsize = bond_potentials.size();
-    const std::size_t bonds_per_thread = bsize / num_threads + 1;
-    std::vector<std::thread> threads;
-    for(std::size_t t=0; t < num_threads-1; ++t)
-    {
-        auto begin = bond_potentials.cbegin() + t * bonds_per_thread;
-        auto end   = bond_potentials.cbegin() + (t+1) * bonds_per_thread;
-        threads.push_back(std::thread([this, &pcon, begin, end](){
-                calc_bond_force(pcon, begin, end);}));
-    }
-    {// last one is executed "this" thread.
-        auto begin = bond_potentials.cbegin() + (num_threads - 1) * bonds_per_thread;
-        auto end = bond_potentials.cend();
-        calc_bond_force(pcon, begin, end);
-    }
-    std::for_each(threads.begin(), threads.end(), [](std::thread& t){t.join();});
-
-// angle -----------------------------------------------------------------------
-    const std::size_t asize = angle_potentials.size();
-    const std::size_t angles_per_thread = asize / num_threads + 1;
-    for(std::size_t t=0; t < num_threads-1; ++t)
-    {
-        auto begin = angle_potentials.cbegin() + t     * angles_per_thread;
-        auto end   = angle_potentials.cbegin() + (t+1) * angles_per_thread;
-        threads.at(t) = std::thread([this, &pcon, begin, end](){
-                calc_angle_force(pcon, begin, end);});
-    }
-    {// last one is executed "this" thread.
-        auto begin = angle_potentials.cbegin() + (num_threads - 1) * angles_per_thread;
-        auto end   = angle_potentials.cend();
-        calc_angle_force(pcon, begin, end);
-    }
-    std::for_each(threads.begin(), threads.end(), [](std::thread& t){t.join();});
-
-// dihd ------------------------------------------------------------------------
-    const std::size_t dsize = dihd_potentials.size();
-    const std::size_t dihds_per_thread = dsize / num_threads + 1;
-    for(std::size_t t=0; t < num_threads-1; ++t)
-    {
-        auto begin = dihd_potentials.cbegin() + t     * dihds_per_thread;
-        auto end   = dihd_potentials.cbegin() + (t+1) * dihds_per_thread;
-        threads.at(t) = std::thread([this, &pcon, begin, end](){
-                calc_dihd_force(pcon, begin, end);});
-    }
-    {// last one is executed "this" thread.
-        auto begin = dihd_potentials.cbegin() + (num_threads - 1) * dihds_per_thread;
-        auto end   = dihd_potentials.cend();
-        calc_dihd_force(pcon, begin, end);
-    }
-
-    std::for_each(threads.begin(), threads.end(), [](std::thread& t){t.join();});
-    return;
-}
-#else // built as single core version
-template<typename traitsT>
-void LocalForceField<traitsT>::calc_force(particle_container_type& pcon)
+void LocalForceField<traitsT>::calc_force(particle_container_type& pcon) const
 {
     calc_bond_force(pcon, bond_potentials.cbegin(),  bond_potentials.cend());
     calc_angle_force(pcon, angle_potentials.cbegin(), angle_potentials.cend());
     calc_dihd_force(pcon, dihd_potentials.cbegin(),  dihd_potentials.cend());
     return;
 }
-#endif
 
-#ifdef MJOLNIR_PARALLEL_THREAD
-template<typename traitsT>
-typename LocalForceField<traitsT>::real_type
-LocalForceField<traitsT>::calc_energy(const particle_container_type& pcon) const
-{
-    std::vector<real_type> Es(num_threads);
-// bond ------------------------------------------------------------------------
-    const std::size_t bsize = bond_potentials.size();
-    const std::size_t bonds_per_thread = bsize / num_threads + 1;
-    std::vector<std::thread> threads;
-    for(std::size_t t=0; t < num_threads-1; ++t)
-    {
-        auto begin = bond_potentials.cbegin() + t * bonds_per_thread;
-        auto end   = bond_potentials.cbegin() + (t+1) * bonds_per_thread;
-        real_type& e = Es.at(t);
-        threads.push_back(std::thread([this, &pcon, begin, end, &Es, t](){
-                Es.at(t) = calc_bond_energy(pcon, begin, end);}));
-    }
-    {// last one is executed "this" thread.
-        auto begin = bond_potentials.cbegin() + (num_threads - 1) * bonds_per_thread;
-        auto end = bond_potentials.cend();
-        Es.at(num_threads-1) = calc_bond_energy(pcon, begin, end);
-    }
-    std::for_each(threads.begin(), threads.end(), [](std::thread& t){t.join();});
-
-// angle -----------------------------------------------------------------------
-    const std::size_t asize = angle_potentials.size();
-    const std::size_t angles_per_thread = asize / num_threads + 1;
-    for(std::size_t t=0; t < num_threads-1; ++t)
-    {
-        auto begin = angle_potentials.cbegin() + t     * angles_per_thread;
-        auto end   = angle_potentials.cbegin() + (t+1) * angles_per_thread;
-        real_type& e = Es.at(t);
-        threads.at(t) = std::thread([this, &pcon, begin, end, &Es, t](){
-                Es.at(t) += calc_angle_energy(pcon, begin, end);});
-    }
-    {// last one is executed "this" thread.
-        auto begin = angle_potentials.cbegin() + (num_threads - 1) * angles_per_thread;
-        auto end   = angle_potentials.cend();
-        Es.at(num_threads-1) += calc_angle_energy(pcon, begin, end);
-    }
-    std::for_each(threads.begin(), threads.end(), [](std::thread& t){t.join();});
-
-// dihd ------------------------------------------------------------------------
-    const std::size_t dsize = dihd_potentials.size();
-    const std::size_t dihds_per_thread = dsize / num_threads + 1;
-    for(std::size_t t=0; t < num_threads-1; ++t)
-    {
-        auto begin = dihd_potentials.cbegin() + t     * dihds_per_thread;
-        auto end   = dihd_potentials.cbegin() + (t+1) * dihds_per_thread;
-        threads.at(t) = std::thread([this, &pcon, begin, end, &Es, t](){
-                Es.at(t) += calc_dihd_energy(pcon, begin, end);});
-    }
-    {// last one is executed "this" thread.
-        auto begin = dihd_potentials.cbegin() + (num_threads - 1) * dihds_per_thread;
-        auto end   = dihd_potentials.cend();
-        Es.at(num_threads-1) += calc_dihd_energy(pcon, begin, end);
-    }
-
-    std::for_each(threads.begin(), threads.end(), [](std::thread& t){t.join();});
-
-    real_type E=0.;
-    std::for_each(Es.cbegin(), Es.cend(), [&E](real_type e){E += e;});
-    return E;
-}
-#else // single core version
 template<typename traitsT>
 typename LocalForceField<traitsT>::real_type
 LocalForceField<traitsT>::calc_energy(const particle_container_type& pcon) const
@@ -332,13 +200,12 @@ LocalForceField<traitsT>::calc_energy(const particle_container_type& pcon) const
         calc_dihd_energy(
             pcon, dihd_potentials.cbegin(),  dihd_potentials.cend());
 }
-#endif
 
 template<typename traitsT>
 inline void LocalForceField<traitsT>::calc_bond_force(
         particle_container_type& pcon,
         typename bond_container::const_iterator iter,
-        const typename bond_container::const_iterator end)
+        const typename bond_container::const_iterator end) const
 {
     for(; iter != end; ++iter)
         bond.calc_force(pcon.at(iter->i), pcon.at(iter->j), *(iter->pot));
@@ -349,7 +216,7 @@ template<typename traitsT>
 inline void LocalForceField<traitsT>::calc_angle_force(
         particle_container_type& pcon,
         typename angle_container::const_iterator iter,
-        const typename angle_container::const_iterator end)
+        const typename angle_container::const_iterator end) const
 {
     for(; iter != end; ++iter)
         angle.calc_force(pcon.at(iter->i), pcon.at(iter->j), pcon.at(iter->k),
@@ -361,7 +228,7 @@ template<typename traitsT>
 inline void LocalForceField<traitsT>::calc_dihd_force(
         particle_container_type& pcon,
         typename dihd_container::const_iterator iter,
-        const typename dihd_container::const_iterator end)
+        const typename dihd_container::const_iterator end) const
 {
     for(; iter != end; ++iter)
         dihd.calc_force(pcon.at(iter->i), pcon.at(iter->j), pcon.at(iter->k),
@@ -412,6 +279,137 @@ LocalForceField<traitsT>::calc_dihd_energy(
                 pcon.at(iter->l), *(iter->pot));
     return energy;
 }
+
+// parallel impl----------------------------------------------------------------
+#ifdef MJOLNIR_PARALLEL_THREAD
+template<typename traitsT>
+void LocalForceField<traitsT>::calc_force(particle_container_type& pcon,
+        const std::size_t num_threads) const
+{
+    std::vector<std::future<void>> futures(num_threads-1);
+
+// bond ------------------------------------------------------------------------
+    const std::size_t bsize = bond_potentials.size();
+    const std::size_t bonds_per_thread = bsize / num_threads + 1;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        auto begin = bond_potentials.cbegin() + t * bonds_per_thread;
+        auto end   = bond_potentials.cbegin() + (t+1) * bonds_per_thread;
+        futures.at(t) = std::async(std::launch::async,
+            [this, &pcon, begin, end](){calc_bond_force(pcon, begin, end);});
+    }
+    {// last one is executed "this" thread.
+        auto begin = bond_potentials.cbegin() + (num_threads - 1) * bonds_per_thread;
+        auto end = bond_potentials.cend();
+        calc_bond_force(pcon, begin, end);
+    }
+    for(std::size_t t = 0; t < num_threads-1; ++t)
+        futures.at(t).get();
+
+// angle -----------------------------------------------------------------------
+    const std::size_t asize = angle_potentials.size();
+    const std::size_t angles_per_thread = asize / num_threads + 1;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        auto begin = angle_potentials.cbegin() + t     * angles_per_thread;
+        auto end   = angle_potentials.cbegin() + (t+1) * angles_per_thread;
+        futures.at(t) = std::async(std::launch::async,
+            [this, &pcon, begin, end](){calc_angle_force(pcon, begin, end);});
+    }
+    {// last one is executed "this" thread.
+        auto begin = angle_potentials.cbegin() + (num_threads - 1) * angles_per_thread;
+        auto end   = angle_potentials.cend();
+        calc_angle_force(pcon, begin, end);
+    }
+    for(std::size_t t = 0; t < num_threads-1; ++t)
+        futures.at(t).get();
+
+// dihd ------------------------------------------------------------------------
+    const std::size_t dsize = dihd_potentials.size();
+    const std::size_t dihds_per_thread = dsize / num_threads + 1;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        auto begin = dihd_potentials.cbegin() + t     * dihds_per_thread;
+        auto end   = dihd_potentials.cbegin() + (t+1) * dihds_per_thread;
+        futures.at(t) = std::async(std::launch::async,
+            [this, &pcon, begin, end](){calc_dihd_force(pcon, begin, end);});
+    }
+    {// last one is executed "this" thread.
+        auto begin = dihd_potentials.cbegin() + (num_threads - 1) * dihds_per_thread;
+        auto end   = dihd_potentials.cend();
+        calc_dihd_force(pcon, begin, end);
+    }
+    for(std::size_t t = 0; t < num_threads-1; ++t)
+        futures.at(t).get();
+
+    return;
+}
+
+template<typename traitsT>
+typename LocalForceField<traitsT>::real_type
+LocalForceField<traitsT>::calc_energy(const particle_container_type& pcon,
+        const std::size_t num_threads) const
+{
+    real_type energy = 0.;
+    std::vector<std::future<real_type>> futures(num_threads-1);
+
+// bond ------------------------------------------------------------------------
+    const std::size_t bsize = bond_potentials.size();
+    const std::size_t bonds_per_thread = bsize / num_threads + 1;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        auto begin = bond_potentials.cbegin() + t * bonds_per_thread;
+        auto end   = bond_potentials.cbegin() + (t+1) * bonds_per_thread;
+        futures.at(t) = std::async(std::launch::async,
+            [this, &pcon, begin, end](){return calc_bond_energy(pcon, begin, end);});
+    }
+    {// last one is executed "this" thread.
+        auto begin = bond_potentials.cbegin() + (num_threads - 1) * bonds_per_thread;
+        auto end = bond_potentials.cend();
+        energy += calc_bond_energy(pcon, begin, end);
+    }
+    for(std::size_t t = 0; t < num_threads-1; ++t)
+        energy += futures.at(t).get();
+
+// angle -----------------------------------------------------------------------
+    const std::size_t asize = angle_potentials.size();
+    const std::size_t angles_per_thread = asize / num_threads + 1;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        auto begin = angle_potentials.cbegin() + t     * angles_per_thread;
+        auto end   = angle_potentials.cbegin() + (t+1) * angles_per_thread;
+        futures.at(t) = std::async(std::launch::async,
+            [this, &pcon, begin, end](){return calc_angle_energy(pcon, begin, end);});
+    }
+    {// last one is executed "this" thread.
+        auto begin = angle_potentials.cbegin() + (num_threads - 1) * angles_per_thread;
+        auto end   = angle_potentials.cend();
+        energy += calc_angle_energy(pcon, begin, end);
+    }
+    for(std::size_t t = 0; t < num_threads-1; ++t)
+        energy += futures.at(t).get();
+
+// dihd ------------------------------------------------------------------------
+    const std::size_t dsize = dihd_potentials.size();
+    const std::size_t dihds_per_thread = dsize / num_threads + 1;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        auto begin = dihd_potentials.cbegin() + t     * dihds_per_thread;
+        auto end   = dihd_potentials.cbegin() + (t+1) * dihds_per_thread;
+        futures.at(t) = std::async(std::launch::async,
+            [this, &pcon, begin, end](){return calc_dihd_energy(pcon, begin, end);});
+    }
+    {// last one is executed "this" thread.
+        auto begin = dihd_potentials.cbegin() + (num_threads - 1) * dihds_per_thread;
+        auto end   = dihd_potentials.cend();
+        energy += calc_dihd_energy(pcon, begin, end);
+    }
+    for(std::size_t t = 0; t < num_threads-1; ++t)
+        energy += futures.at(t).get();
+
+    return energy;
+}
+#endif
 
 } // mjolnir
 #endif /* MJOLNIR_LOCAL_FORCE_FIELD */
