fe586fd8f28dc53cffc6c8ea45ba091f559d66b1
ToruNiina <niina.toru.68u@gmail.com>
2017-01-13 15:43:19 +0900

    add spin_lock

---
+++ mjolnir/util/spin_lock.hpp
@@ -0,0 +1,29 @@
+#ifndef MJOLNIR_SPIN_LOCK
+#define MJOLNIR_SPIN_LOCK
+#include <atomic>
+
+namespace mjolnir
+{
+
+class spinlock
+{
+  public:
+    spinlock(): state_(ATOMIC_FLAG_INIT){}
+    ~spinlock() = default;
+
+    void lock()
+    {
+        while(state_.test_and_set(std::memory_order_acquire)){/* spin! */;}
+    }
+
+    void unlock()
+    {
+        state_.clear(std::memory_order_release);
+    }
+
+  private:
+    std::atomic_flag state_;
+};
+
+} // mjolnir
+#endif /* MJOLNIR_SPIN_LOCK */
