aad42046b35cde2d699c1b08d9c342c23f3368f3
ToruNiina <niina.toru.68u@gmail.com>
2017-01-13 18:01:57 +0900

    add parallel impl to VelocityVerlet

--- mjolnir/core/VelocityVerlet.hpp
+++ mjolnir/core/VelocityVerlet.hpp
@@ -24,11 +24,24 @@ class VelocityVerlet : public Integrator<traitsT>
     ~VelocityVerlet() override = default;
 
     void initialize(const ParticleContainer<traitsT>& pcon) override;
+
     time_type step(const time_type time, ParticleContainer<traitsT>& pcon,
-                   ForceField<traitsT>& ff) override;
+                   const ForceField<traitsT>& ff) override;
 
     time_type& delta_t()       override {return dt_;}
     time_type  delta_t() const override {return dt_;}
+
+#ifdef MJOLNIR_PARALLEL_THREAD
+    time_type step(const time_type time, ParticleContainer<traitsT>& pcon,
+       const ForceField<traitsT>& ff, const std::size_t num_threads) override;
+
+  private:
+    void step_pos_and_half_vel(ParticleContainer<traitsT>& pcon,
+            std::size_t index, const std::size_t end);
+    void step_acc_and_half_vel(ParticleContainer<traitsT>& pcon,
+            std::size_t index, const std::size_t end);
+
+#endif // MJOLNIR_PARALLEL_THREAD
 
   private:
     time_type dt_;      //!< dt
@@ -52,7 +65,7 @@ void VelocityVerlet<traitsT>::initialize(const ParticleContainer<traitsT>& pcon)
 template<typename traitsT>
 typename VelocityVerlet<traitsT>::time_type
 VelocityVerlet<traitsT>::step(const time_type time,
-        ParticleContainer<traitsT>& pcon, ForceField<traitsT>& ff)
+        ParticleContainer<traitsT>& pcon, const ForceField<traitsT>& ff)
 {
     // calc r(t+dt)
     for(auto iter = make_zip(pcon.begin(), acceleration_.cbegin());
@@ -79,6 +92,81 @@ VelocityVerlet<traitsT>::step(const time_type time,
 
     return time + dt_;
 }
+
+#ifdef MJOLNIR_PARALLEL_THREAD
+template<typename traitsT>
+typename VelocityVerlet<traitsT>::time_type
+VelocityVerlet<traitsT>::step(const time_type time,
+        ParticleContainer<traitsT>& pcon, const ForceField<traitsT>& ff,
+        const std::size_t num_threads)
+{
+    std::vector<std::future<void>> futures(num_threads-1);
+
+    // calc r(t+dt)
+    const std::size_t num_particle = pcon.size();
+    const std::size_t particle_per_thread = num_particle / num_threads + 1;
+    std::size_t begin = 0;
+    std::size_t end = particle_per_thread;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        futures[t] = std::async(std::launch::async,
+            [this, &pcon, begin, end](){step_pos_and_half_vel(pcon, begin, end);});
+
+        begin += particle_per_thread;
+        end   += particle_per_thread;
+    }
+    step_pos_and_half_vel(pcon, begin, num_particle);
+    for(auto iter = futures.begin(); iter != futures.end(); ++iter)
+        iter->get();
+
+    // calc f(t+dt)
+    ff.calc_force(pcon, num_threads);
+
+    // calc a(t+dt) and v(t+dt)
+    begin = 0;
+    end = particle_per_thread;
+    for(std::size_t t=0; t < num_threads-1; ++t)
+    {
+        futures[t] = std::async(std::launch::async,
+            [this, &pcon, begin, end](){step_acc_and_half_vel(pcon, begin, end);});
+
+        begin += particle_per_thread;
+        end   += particle_per_thread;
+    }
+    step_pos_and_half_vel(pcon, begin, num_particle);
+    for(auto iter = futures.begin(); iter != futures.end(); ++iter)
+        iter->get();
+
+    return time + dt_;
+}
+
+template<typename traitsT>
+void VelocityVerlet<traitsT>::step_pos_and_half_vel(
+        ParticleContainer<traitsT>& pcon, std::size_t i, const std::size_t end)
+{
+    for(; i != end; ++i)
+    {
+        pcon[i].position += dt_ * pcon[i].velocity + halfdt2_ * acceleration_[i];
+        pcon[i].velocity += halfdt_ * acceleration_[i];
+    }
+    return;
+}
+
+template<typename traitsT>
+void VelocityVerlet<traitsT>::step_acc_and_half_vel(
+        ParticleContainer<traitsT>& pcon, std::size_t i, const std::size_t end)
+{
+    for(; i != end; ++i)
+    {
+        const coordinate_type acc = pcon[i].force / pcon[i].mass;
+        acceleration_[i] = acc;
+        pcon[i].velocity += halfdt_ * acc;
+        pcon[i].force = coordinate_type(0., 0., 0.);
+    }
+    return;
+}
+
+#endif
 
 } // mjolnir
 #endif /* MJOLNIR_VELOCITY_VERLET_INTEGRATOR */
