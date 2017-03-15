e798a36ef434ff6320573cd21d6d42844d9de2e7
ToruNiina <niina.toru.68u@gmail.com>
2017-03-15 20:51:05 +0900

    add mockup for SIMD operation detection

--- megingjord/simd/pack.hpp
+++ megingjord/simd/pack.hpp
@@ -38,15 +38,35 @@ struct single_type_of<packed_array<T, N, trait>>
 #include "functors.hpp"
 
 #ifdef MJOLNIR_HAVE_AVX
+// __m256, __m256d
 #include <immintrin.h>
 #include "avx/pack_avx.hpp"
 #include "avx/functor_avx.hpp"
-#define MEGINGJORD_DEFAULT_SIMD avx_traits
+#endif// avx
+
+#ifdef MJOLNIR_HAVE_AVX2
+// __m256i and FMA operation
+// TODO #include "avx2/pack_avx2.hpp"
+//      #include "avx2/functor_avx2.hpp"
+#endif// avx2
+
+#ifdef MJOLNIR_HAVE_FMA
+#include "fma/functor_fma.hpp"
+#elif defined(MJOLNIR_HAVE_AVX)
+// TODO #include "avx/fallback_fma.hpp"
+#elif defined(MJOLNIR_HAVE_SSE4_2)
+// TODO #include "sse/fallback_fma.hpp"
 #endif
 
-#ifndef MJOLNIR_HAVE_AVX
-#include "plain/pack_plain.hpp"
-#define MEGINGJORD_DEFAULT_SIMD plain_traits
+// XXX set default simd operation
+// TODO: AVX512
+#if defined(MJOLNIR_HAVE_AVX2) || defined(MJOLNIR_HAVE_AVX)
+#    define MEGINGJORD_DEFAULT_SIMD avx_traits
+// TODO: #elif defined(MJOLNIR_HAVE_SSE4_2)
+//       #    define MEGINGJORD_DEFAULT_SIMD sse_traits
+#else // fallback for no known SIMD
+#    include "plain/pack_plain.hpp"
+#    define MEGINGJORD_DEFAULT_SIMD plain_traits
 #endif
 
 #include "packable_array.hpp"
